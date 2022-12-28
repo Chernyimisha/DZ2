6948fabfce4ba3a84ee89030061a49fa36a86b88
Proshkin Mikhail <chernyimisha@rambler.ru>
2022-12-29 00:07:31 +0300

    Выполнил указания преподавателя-дравствуйте, Михаил!

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -2,11 +2,12 @@
 вход трёхзначное число и на выходе показывает вторую
 цифру этого числа.*/
 
-Console.WriteLine ("Введите трехзначное число");
-int number = Convert.ToInt32 (Console.ReadLine());
-int number1 = number % 100;
-int result = number1/10;
-Console.WriteLine ($"Вторая цифра числа {number} равна {result}");
-
-
-
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if ( number > 99 && number < 1000)
+{
+    int number1 = number % 100;
+    int result = number1 / 10;
+    Console.WriteLine($"Вторая цифра числа {number} равна {result}");
+}
+else Console.WriteLine("Ввели неправильное число");
--- Task13/Program.cs
+++ Task13/Program.cs
@@ -5,7 +5,15 @@
 Console.WriteLine("Введите целое число");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = number;
-if (number > 99)
+if (number < -99 )
+{
+    while (count < -999 )
+    {
+        count = count / 10;
+    }
+    Console.WriteLine($"Третьей цифрa числа {number} равна {count % 10 * -1}");
+}
+else if ( number > 99)
 {
     while (count > 999)
     {
